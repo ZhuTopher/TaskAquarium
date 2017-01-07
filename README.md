fea00537a3d0fb2990f018f8921dbe9ed7794266
Chris Zhu <christopher.zhu.9@gmail.com>
2017-01-07 14:35:07 -0500

    Clear git cache

--- README.md
+++
@@ -1,3 +0,0 @@
-#TaskAquarium
-
-A fun goal-oriented task management app, with a focus on habit-forming via managing a pseudo aquarium.
--- android/app/src/main/java/chris/TaskAquarium/Managers/TasksManager.java
+++
@@ -1,9 +0,0 @@
-package chris.TaskAquarium.Managers;
-
-/**
- * Created by chris on 8/11/16.
- */
-
-public class TasksManager {
-	// TODO: singleton static class used for saving & loading tasks (saved data)
-}
